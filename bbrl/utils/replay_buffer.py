797544e7c51cc978bb3187455a4cb474dfe56320
Garance LUCAS <125577923+GaranceLucas@users.noreply.github.com>
2023-03-17 13:53:20 +0100

    class PrioritizedReplayBuffer fixed

--- bbrl/utils/replay_buffer.py
+++ bbrl/utils/replay_buffer.py
@@ -119,6 +119,7 @@ class ReplayBuffer:
         n_vars = {k: v.to(device) for k, v in self.variables.items()}
         self.variables = n_vars
 
+
 class PrioritizedReplayBuffer:
     def __init__(self, max_size, device=torch.device("cpu")):
         self.max_size = int(max_size)
@@ -126,8 +127,8 @@ class PrioritizedReplayBuffer:
         self.position = 0
         self.is_full = False
         self.device = device
-        self.best_reward = 1
-
+        self.gamma = 0.9
+        self.lambda_exp = 0.2
 
     def init_workspace(self, all_tensors):
         """
@@ -135,12 +136,12 @@ class PrioritizedReplayBuffer:
         shape of stores tensors : [key] => [self.max_size][time_size][key_dim]
         Makes a copy of the input content
         """
-        #if the variables are not already set, we set them
+        # if the variables are not already set, we set them
         if self.variables is None:
             self.variables = {}
             for k, v in all_tensors.items():
-                #in order to solve the problem of the end of an episode and the beginning of another,
-                #we replace single step informations with transition informations
+                # in order to solve the problem of the end of an episode and the beginning of another,
+                # we replace single step informations with transition informations
                 s = list(v.size())
                 s[1] = self.max_size
                 _s = copy.deepcopy(s)
@@ -152,18 +153,16 @@ class PrioritizedReplayBuffer:
             self.is_full = False
             self.position = 0
 
-
-    #function which insert the value v into the variable dictionnary of the replay buffer
+    # function which insert the value v into the variable dictionnary of the replay buffer
     def _insert(self, k, indexes, v):
         self.variables[k][indexes] = v.detach().moveaxis((0, 1), (1, 0))
 
-
     def put(self, workspace):
         """
-        Add a the content of a workspace to the replay buffer.
+        Add the content of a workspace to the replay buffer.
         The given workspace must have keys of shape : [time_size][batch_size][key_dim]
         """
-        #recovery of position from the workspace
+        # recovery of position from the workspace
         new_data = {
             k: workspace.get_full(k).detach().to(self.device) for k in workspace.keys()
         }
@@ -175,16 +174,17 @@ class PrioritizedReplayBuffer:
 
         for k, v in new_data.items():
             if batch_size is None:
-                batch_size = v.size()[1]  #if the batch_size is not set, we set it equal to the size of the variable of the
-                #workspace that we are adding in the Replay Buffer (new_data corresponds to the dictionnary of the
-                #workspace related to variables, see the file workspace.py, line 330 in the __init__)
+                batch_size = v.size()[
+                    1]  # if the batch_size is not set, we set it equal to the size of the variable of the
+                # workspace that we are adding in the Replay Buffer (new_data corresponds to the dictionnary of the
+                # workspace related to variables, see the file workspace.py, line 330 in the __init__)
 
                 # print(f"{k}: batch size : {batch_size}")
                 # print("pos", self.position)
 
-            #we check if we can add a batch of the size of batch_size in the replay buffer
+            # we check if we can add a batch of the size of batch_size in the replay buffer
             if self.position + batch_size < self.max_size:
-                #the case where the batch can be inserted before the end of the replay buffer
+                # the case where the batch can be inserted before the end of the replay buffer
                 if indexes is None:
                     indexes = torch.arange(batch_size) + self.position
                     arange = torch.arange(batch_size)
@@ -219,77 +219,61 @@ class PrioritizedReplayBuffer:
                 arange = arange.to(dtype=torch.long, device=v.device)
                 self._insert(k, indexes, v)
 
-
-    #function which gives the size of the replay buffer
+    # function which gives the size of the replay buffer
     def size(self):
         if self.is_full:
             return self.max_size
         else:
             return self.position
 
-
-    #function which print the observations
+    # function which print the observations
     def print_obs(self):
         print(f"position: {self.position}")
         print(self.variables["env/env_obs"])
 
-
-    #A MODIFIER POUR NE PLUS PRENDRE LES BATCH RANDOMLY MAIS EN FONCTION DE L'EMPLACEMENT DU BATCH DANS LE REPLAY BUFFER
-    #LES BATCHS LES PLUS A GAUCHE (CEUX SONT LES PLUS PERTINENTS POUR L'APPRENTISSAGE) ONT UNE PROBA ELEVEE D'ÊTRE TIRES
-    #AU LIEU DE PRENDRE LES BATCH RANDOMLY, ON LES TIRE SELON UNE DISTRIBUTION TIREE VERS LA GAUCHE
-    #LOI DE POISSON AVEC 0<alpha<=1
-
-    #function which allows to get the batchs picked according to a Poisson distribution from the replay buffer (under the form of workspace)
     def get_shuffled(self, batch_size):
-        rates = torch.rand(size=1)*batch_size  #rate parameter between 0 and 1 (of the size of the batch_size)
-        who = torch.poisson(rates) #torch.poisson alows to pick a tensor with a probability distribution shifted
-                                   #in order to have a higher probability for a batch which is more relevant for the learning
-        workspace = Workspace() #the tensor 'who' contains thus the INDEXS of the values of the replay buffer, picked according to the Poisson distribution
-        #for k in self.variables:
-            #workspace.set_full(k, self.variables[k][].transpose(0, 1))  #a ajuster !
-
-        return workspace
-
-
-    def get_prioritized(self, batch_size):
-        workspace=Workspace()
-        rewards = []
-        samples_priorities = []
-        batch = []
-
-        for k in self.variables:
-            reward = variable[k]['reward']  #quel emplacement ?
-            rewards.append(reward)  #on récupère les rewards de chaque sample
-        self.best_reward = max(rewards)  #on récupère le plus grand reward
-
-        for k in self.variables:
-            reward = variable[k]["reward"]
-            priority = reward/self.best_reward  #pour chaque sample on définit la priorité en fonction du rapport entre
-                                                #son reward et le meilleur reward -> 0<priorities<1
-            samples_priorities.append({'sample': k, 'priority': priority})  #on associe à chaque sample sa priorité et on
-                                                                            #regroupe tous les couples dans un dictionnaire
-                    #liste de dictionnaire et non de tuples car si besoin on peut changer la valeur de la priorité
-
-        samples_priorities.sort(key=['priority'])  #on trie la liste des dictionnaires en fonction des priorités
-        #afin d'effectuer par la suite un tirage avec une proba plus élevée sur les samples avec une plus grande priorité
-
-
-
-        #IDEE 1:
-        for i in range(batch_size):  #on créé un batch selon la taille désirée en entrée
-            batch.append(samples_priorities[i])
-
-        return batch  #on renvoit le batch
-
-        #IDEE 2:
+        who = torch.randint(
+            low=0, high=self.size(), size=(batch_size,), device=self.device
+        )
         workspace = Workspace()
-        #for k in samples_priorities:
-         #   workspace.set_full(k, samples_priorities[k][].transpose(0, 1))  #a ajuster !
+        for k in self.variables:
+            workspace.set_full(k, self.variables[k][who].transpose(0, 1))
 
         return workspace
 
+    # function which allows to get the batchs picked according to an Exponential distribution from the replay buffer (under the form of workspace)
+    def get_prioritized(self, batch_size):
+        workspace = Workspace()
+        temporal_differences = []
+        batch = [0] * batch_size
 
+        # compute the temporal differences for all the samples
+        for k in range(len(self.variables['env/reward'])):
+            reward = self.variables['env/reward'][k][0]
+            next_reward = self.variables['env/reward'][k][1]
+            q_value = self.variables['q_values'][k][0]
+            q_value_next = self.variables['q_values'][k][1]
+            action = self.variables['action'][k]
+            temporal_differences.append(
+                reward + self.gamma * max(q_value_next) - q_value[action[0]])
 
+        #get the sorted list according to the temporal differences and the indexes
+        tds, indexes = torch.sort(torch.tensor(temporal_differences))
+
+        # batchs creation in order to writte them into the workspace
+        for i in range(batch_size):
+            chosen_index = int(np.random.exponential(scale=self.lambda_exp, size=None))
+            while chosen_index > batch_size:
+                chosen_index = int(np.random.exponential(scale=self.lambda_exp, size=None))
+            batch[i] = tds[chosen_index]
+
+        who = torch.tensor(batch)
+
+        workspace = Workspace()
+        for k in self.variables:
+            workspace.set_full(k, self.variables[k][who].transpose(0, 1))
+
+        return workspace
 
     def to(self, device):
         n_vars = {k: v.to(device) for k, v in self.variables.items()}
